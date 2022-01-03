4cf13d7362adf67c63e7fd8d7eaa4de930e64404
yuvarani03 <yuvarvh1998@gmail.com>
2022-01-03 11:53:02 +0530

    done

---
+++ pronote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './components/Editor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Editor/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Editor.css';
+export default class Editor extends Component{
+    constructor(props) {
+        super(props);
+        // to bind the method with event handler without (). 
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state =
+         { 
+            value: ''
+        };
+    }
+    render(){
+    return(
+        <div className="container">
+         <div className="input">
+            <h3>Input</h3>
+            <textarea className="input-text" onChange={this.handleChange} defaultValue={this.state.value}/>
+         </div>
+         <div className="output">
+            <h3>Pro Note</h3>
+            <div className="output-text">{this.state.value}</div>
+         </div>                
+        </div>
+        )
+    }  
+//event handler method to change the state.
+//setState is used to change the state.
+  handleChange(e){
+    this.setState({value: e.target.value});
+} 
+} 
